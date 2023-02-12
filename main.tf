12f9bde88e4e89dc167002d67ce25d3825d59ffd
madhuboyin <madhuboyina@gmail.com>
2023-02-11 19:37:34 -0500

    updated main.tf

--- main.tf
+++ main.tf
@@ -1,40 +1,64 @@
 provider "aws" {
-  access_key = ""
-  secret_key = ""
-  region     = ""
+  access_key = "AKIA4TCO6NG7MVLP2NF6"
+  secret_key = "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"
+  region     = "us-east-1"
 }
-
-
 data "aws_ssm_parameter" "ami" {
-  name = ""
-
+  name = "/aws/service/ami-amazon-linux-latest/amzn2-ami-hvm-x86_64-gp2"
 }
-
 resource "aws_vpc" "vpc" {
-  cidr_block           = ""
-  enable_dns_hostnames = ""
+  cidr_block           = "10.0.0.0/16"
+  enable_dns_hostnames = "true"
 
 }
-
 resource "aws_internet_gateway" "igw" {
   vpc_id = aws_vpc.vpc.id
-
 }
-
 resource "aws_subnet" "subnet1" {
-
-  cidr_block              = ""
+  cidr_block              = "10.0.0.0/16"
   vpc_id                  = aws_vpc.vpc.id
-  map_public_ip_on_launch = ""
+  map_public_ip_on_launch = "true"
+}
+resource "aws_route_table" "rtb" {
+  vpc_id = aws_vpc.vpc.id
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.igw.id
+  }
+}
+resource "aws_route_table_association" "rta-subnet1" {
+  subnet_id      = aws_subnet.subnet1.id
+  route_table_id = aws_route_table.rtb.id
 
 }
-
-resource "aws_route_table" "rtb" {
-
+resource "aws_security_group" "nginx-sg" {
+  name   = "nginx_sg"
   vpc_id = aws_vpc.vpc.id
-
-  route = {
-    cidr_block = ""
+  ingress = {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
   }
+  egress = {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+resource "aws_instance" "nginx1" {
 
+  ami                    = nonsensitive(data.aws_ssm_parameter.ami.name)
+  instance_type          = "t2.micro"
+  subnet_id              = aws_subnet.subnet1.id
+  vpc_security_group_ids = [aws_security_group.nginx-sg.id]
+
+  user_data = <<EOF
+    #! /bin/bash
+    sudo amazon-linux-extras install -y nginx1
+    sudo service nginx start
+    sudo rm /usr/share/nginx/html/index.html
+    echo '<html><head><title>Madhu Boyina server</title></head><body style=\"backgroun-color:1F778D\"><h1>Hello Boyinas<h1></body></html>
+    EOF
 }
